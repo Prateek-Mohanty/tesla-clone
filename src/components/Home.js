1534842cd646c3ce8d68065760a83934366b7154
Prateek Mohanty <mohantyprateekkumar@gmail.com>
2023-01-01 22:41:34 +0530

    images commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,49 +8,49 @@ function Home() {
         <Section 
             title="Model S"
             description="Order Online for Touchless Delivery"
-            backgroundimg="./tesla-clone/model-s.jpg"
+            backgroundimg="model-s.jpg"
             leftbtnText="Custom Order"
             rightbtnText="Existing Inventory"
         />
         <Section 
             title="Model Y"
             description="Order Online for Touchless Delivery"
-            backgroundimg="./tesla-clone/model-y.jpg"
+            backgroundimg="model-y.jpg"
             leftbtnText="Custom Order"
             rightbtnText="Existing Inventory"
         />
         <Section
             title="Model 3"
             description="Order Online for Touchless Delivery"
-            backgroundimg="./tesla-clone/model-x.jpg"
+            backgroundimg="model-x.jpg"
             leftbtnText="Custom Order"
             rightbtnText="Existing Inventory"
         />
         <Section
             title="Model X"
             description="Order Online for Touchless Delivery"
-            backgroundimg="./tesla-clone/model-3.jpg"
+            backgroundimg="model-3.jpg"
             leftbtnText="Custom Order"
             rightbtnText="Existing Inventory"
         />
         <Section
             title="Lowest Cost Solar Panels in America"
             description="Money-back gurantee"
-            backgroundimg="./tesla-clone/solar-panel.jpg"
+            backgroundimg="solar-panel.jpg"
             leftbtnText="Order now"
             rightbtnText="learn More"
         />
         <Section 
             title="Solar for New Roofs"
             description="Solar Roofs Costs less than New Roofs plus Solar Panels"
-            backgroundimg="./tesla-clone/solar-roof.jpg"
+            backgroundimg="solar-roof.jpg"
             leftbtnText="Order now"
             rightbtnText="learn More"
         />
         <Section 
             title="Accessories"
             description=""
-            backgroundimg="./tesla-clone/accessories.jpg"
+            backgroundimg="accessories.jpg"
             leftbtnText="Shop now"
             rightbtnText=""
         />
